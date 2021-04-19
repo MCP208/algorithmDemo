4c53d8edc9750bffa59722845d9acfe182e14c1d
zhoujianping7758521 <448153700@qq.com>
2021-04-19 10:55:41 +0800

    1.第七道算法

---
+++ src/main/java/com/study/offer/OneToTen/Seven.java
@@ -0,0 +1,53 @@
+package com.study.offer.OneToTen;
+
+/**
+ * @author ZJP
+ * @create 2021-04-16 10:53
+ */
+public class Seven {
+    /**
+     * 写一个函数，输入 n ，求斐波那契（Fibonacci）数列的第 n 项（即 F(N)）。斐波那契数列的定义如下：
+     * <p>
+     * F(0) = 0,   F(1) = 1
+     * F(N) = F(N - 1) + F(N - 2), 其中 N > 1.
+     * 斐波那契数列由 0 和 1 开始，之后的斐波那契数就是由之前的两数相加而得出。
+     * <p>
+     * 答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：n = 2
+     * 输出：1
+     * 示例 2：
+     * <p>
+     * 输入：n = 5
+     * 输出：5
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 0 <= n <= 100
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/fei-bo-na-qi-shu-lie-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+    public static void main(String[] args) {
+        //这个也不是很懂 没有读懂题意坐等春鹏大佬解释
+        int n = 100;
+        int i = SevenZjp(n);
+        System.out.println("斐波那契数为："+i);
+    }
+
+    private static int SevenZjp(int n) {
+        int a = 0, b = 1, sum;
+        for (int i = 0; i < n; i++) {
+            sum = (a + b) % 1000000007;
+            a = b;
+            b = sum;
+        }
+        return a;
+    }
+}
