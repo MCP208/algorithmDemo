94b8ce85713f1dd91de57734a4ae906160ad3deb
zhoujianping7758521 <448153700@qq.com>
2021-04-22 09:48:10 +0800

    1.第十道算法

---
+++ src/main/java/com/study/offer/OneToTen/Ten.java
@@ -0,0 +1,77 @@
+package com.study.offer.OneToTen;
+
+/**
+ * @author ZJP
+ * @create 2021-04-21 10:35
+ */
+public class Ten {
+    /**
+     * 给定一个 m x n 二维字符网格 board 和一个字符串单词 word 。如果 word 存在于网格中，返回 true ；否则，返回 false 。
+     * <p>
+     * 单词必须按照字母顺序，通过相邻的单元格内的字母构成，其中“相邻”单元格是那些水平相邻或垂直相邻的单元格。同一个单元格内的字母不允许被重复使用。
+     * <p>
+     *  
+     * <p>
+     * 例如，在下面的 3×4 的矩阵中包含单词 "ABCCED"（单词中的字母已标出）。
+     * <p>
+     * <p>
+     * <p>
+     *  
+     * <p>
+     * 示例 1：
+     * <p>
+     * 输入：board = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]], word = "ABCCED"
+     * 输出：true
+     * 示例 2：
+     * <p>
+     * 输入：board = [["a","b"],["c","d"]], word = "abcd"
+     * 输出：false
+     *  
+     * <p>
+     * 提示：
+     * <p>
+     * 1 <= board.length <= 200
+     * 1 <= board[i].length <= 200
+     * board 和 word 仅由大小写英文字母组成
+     * <p>
+     * 来源：力扣（LeetCode）
+     * 链接：https://leetcode-cn.com/problems/ju-zhen-zhong-de-lu-jing-lcof
+     * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+     */
+
+    public static void main(String[] args) {
+       //题意都没有读懂 坐等春鹏大佬解释  算法真是太难了o(╥﹏╥)o
+        char[][] board = {{'A', 'B', 'C', 'E'}, {'S', 'F', 'C', 'S'}, {'A', 'D', 'E', 'E'}};
+        String word = "ABCCED";
+        Solution solution = new Solution();
+        boolean exist = solution.exist(board, word);
+        if(exist){
+            System.out.println("存在网格中！");
+        }else {
+            System.out.println("不存在网格中！");
+        }
+
+    }
+
+    static class Solution {
+        public boolean exist(char[][] board, String word) {
+            char[] words = word.toCharArray();
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board[0].length; j++) {
+                    if (dfs(board, words, i, j, 0)) return true;
+                }
+            }
+            return false;
+        }
+
+        boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+            if (i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+            if (k == word.length - 1) return true;
+            board[i][j] = '\0';
+            boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                    dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i, j - 1, k + 1);
+            board[i][j] = word[k];
+            return res;
+        }
+    }
+}
