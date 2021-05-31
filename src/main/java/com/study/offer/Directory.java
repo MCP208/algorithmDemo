469798212d8a00214d3c3ef08fe4b2deb14badb6
MCP <machunpeng@bonc.com.cn>
2021-05-31 16:53:07 +0800

    题目提交

--- src/main/java/com/study/offer/Directory.java
+++ src/main/java/com/study/offer/Directory.java
@@ -1,5 +1,6 @@
 package com.study.offer;
 
+import com.study.offer.group11_20.*;
 import com.study.offer.group1_10.*;
 
 /**
@@ -41,7 +42,28 @@ public class Directory {
         //剑指 Offer 12	矩阵中的路径
         new A10();
 
+        //剑指 Offer 13. 机器人的运动范围
+        new A11();
 
+        //剑指 Offer 14- I. 剪绳子
+        new A12();
 
+        //剑指 Offer 14- II. 剪绳子 II
+        new A13();
+
+        //剑指 Offer 15. 二进制中1的个数
+        new A14();
+
+        //剑指 Offer 16. 数值的整数次方
+        new A15();
+
+        //剑指 Offer 17. 打印从1到最大的n位数
+        new A18();
+
+        //剑指 Offer 18. 删除链表的节点
+        new A19();
+
+        //剑指 Offer 19. 正则表达式匹配
+        new A20();
     }
 }
---
+++ src/main/java/com/study/offer/group11_20/A17.java
@@ -0,0 +1,11 @@
+package com.study.offer.group11_20;
+
+public class A17 {
+
+    /**
+     *
+     *
+     *
+     *
+     */
+}
